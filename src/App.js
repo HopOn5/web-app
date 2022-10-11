1cf766f9cd52fd4f090ec8d79012bba3e2cbb565
Rizna PK <rizna@Riznas-MacBook-Air.local>
2022-10-11 20:47:20 +0100

    installed MUI

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from "react";
 import "./styles/index.scss";
 import AppRouter from "./router/AppRouter";
 import PageLayout from "./components/PageLayout";
+import { PersonalDetails } from "./pages/onBoarding/personalDetails/PersonalDetails";
 
 const App = () => {
-    return (
-        <div className="root-container">
-            <PageLayout>
-                <AppRouter />
-            </PageLayout>
-        </div>
-    );
+  return (
+    <div className="root-container">
+      <PageLayout>
+        <AppRouter />
+        <PersonalDetails />
+      </PageLayout>
+    </div>
+  );
 };
 
 export default App;
