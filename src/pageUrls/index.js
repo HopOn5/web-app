60748b5cd23c8b9fbb04f1e7cac3905b56b7069b
mahiMathewsCov <mathewsm@uni.coventry.ac.uk>
2022-10-18 16:47:42 +0100

    Merge conflict fix

--- src/pageUrls/index.js
+++ src/pageUrls/index.js
@@ -11,15 +11,6 @@ export const URLData = {
         url: "/registration",
         layout: { isLogo: true, right: { type: "signin" }, title: "Register" }
     },
-    home: { url: "/", isLogo: true, layout: { right: { type: "user" } } },
-    login: {
-        url: "/login",
-        isLogo: true
-    },
-    signin: {
-        url: "/signin",
-        isLogo: true,
-        layout: { right: {}, title: "Sign in" },
     home: { url: "/", layout: { isLogo: true, right: { type: "user" } } },
     signin: {
         url: "/signin",
