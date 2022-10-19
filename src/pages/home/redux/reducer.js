703e68930d6c184a93a25e698a4d11c8735cda6b
mahiMathewsCov <mathewsm@uni.coventry.ac.uk>
2022-10-19 23:27:23 +0100

    mock data removal

--- src/pages/home/redux/reducer.js
+++ src/pages/home/redux/reducer.js
@@ -1,51 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
-    upcomingRoutes: [
-        {
-            start_date: "12/12/1992",
-            start_time: "09:00",
-            end_point: {
-                streetAddress: "18 Nicholls Street",
-                pincode: "CV24GY"
-            },
-            start_point: {
-                streetAddress: "18 Nicholls Street",
-                pincode: "CV24GY"
-            },
-            route_companion: {}
-        },
-        {
-            start_date: "12/12/1992",
-            start_time: "09:00",
-            end_point: {
-                streetAddress: "18 Nicholls Street",
-                pincode: "CV24GY"
-            },
-            start_point: {
-                streetAddress: "18 Nicholls Street",
-                pincode: "CV24GY"
-            },
-            route_companion: {}
-        }
-    ],
-    completedRoutes: [
-        {
-            start_date: "12/12/1992",
-            start_time: "09:00",
-            start_point: {
-                streetAddress: "18 Nicholls Street",
-                pincode: "CV24GY"
-            },
-            end_point: {
-                streetAddress: "18 Nicholls Street",
-                pincode: "CV24GY"
-            },
-            route_companion: {
-                name: "Jack",
-                start_point: { streetAddress: "19 Nicholls Street" }
-            }
-        }
-    ]
+    upcomingRoutes: [],
+    completedRoutes: []
 };
 const homeSlice = createSlice({
     name: "home",
