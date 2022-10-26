ced5cd55434a01d98c24bc617f7af2a0b84bd157
Anuja <83329624+anuja1694@users.noreply.github.com>
2022-10-26 16:10:04 +0100

    authentication forms validation completed

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -4,13 +4,14 @@ import Home from "../pages/home";
 import Onboarding from "../pages/onBoarding";
 import Profile from "../pages/Profile/Profile";
 import Registration from "../pages/registration/Registration";
+import RouteRequests from "../pages/routeRequest";
 import ResetPassword from "../pages/resetpassword/ResetPassword";
 import Signin from "../pages/signin/Signin";
 import { URLData } from "../pageUrls";
 
 // import PrivateRoute from "./PrivateRoute";
 
-const AppRouter = ({ children, ...props }) => {
+const AppRouter = () => {
     return (
         <Routes>
             <Route
@@ -32,6 +33,10 @@ const AppRouter = ({ children, ...props }) => {
             <Route path={URLData.registration.url} element={<Registration />} />
             <Route path={URLData.signin.url} element={<Signin />} />
             <Route path={URLData.profile.url} element={<Profile />} />
+            <Route
+                path={URLData.routeRequest.url}
+                element={<RouteRequests />}
+            />
         </Routes>
     );
 };
