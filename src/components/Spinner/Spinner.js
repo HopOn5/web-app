a78ba935853ee3cec823d4d0d74a30f3b4c6db2e
Anuja <83329624+anuja1694@users.noreply.github.com>
2022-10-31 22:33:19 +0000

    Route flow

---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Spinner() {
+    return (
+        <div className="spinner-container">
+            <div className="loading-spinner">
+
+            </div>
+        </div>
+    );
+}
+
+export default Spinner;
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Router } from "react-router-dom";
 import Home from "../pages/home";
 import Onboarding from "../pages/onBoarding";
 import EmptyProfile from "../pages/Profile/EmptyProfile";
@@ -11,6 +11,7 @@ import Signin from "../pages/signin/Signin";
 import { URLData } from "../pageUrls";
 import { ChatSpace } from "../pages/chatSpace";
 import Googlemap from "../pages/Googlemap/googlemap";
+import PrivateRoute from "../components/PrivateRoute";
 
 const AppRouter = () => {
     return (
@@ -23,7 +24,9 @@ const AppRouter = () => {
                 path={URLData.resetpassword.url}
                 element={<ResetPassword />}
             />
+            <Route path={URLData.home.url} element={<PrivateRoute />}>
             <Route path={URLData.home.url} element={<Home />} />
+            </Route>
             <Route path={URLData.profile.url} element={<Profile />} />
             <Route path={URLData.googlemap.url} element={<Googlemap />} />
 
