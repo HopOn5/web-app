fc7e88c4d81141c5441e787334dceee9f2607148
Rizna PK <rizna@Riznas-MacBook-Air.local>
2022-11-02 14:09:46 +0000

    address validation added

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -10,33 +10,26 @@ import ResetPassword from "../pages/resetpassword/ResetPassword";
 import Signin from "../pages/signin/Signin";
 import { URLData } from "../pageUrls";
 import { ChatSpace } from "../pages/chatSpace";
-import Googlemap from "../pages/Googlemap/googlemap";
 import PrivateRoute from "../components/PrivateRoute";
 
 const AppRouter = () => {
-    return (
-        <Routes>
-            <Route path={URLData.onboarding.url} element={<Onboarding />} />
-            <Route path={URLData.profileEdit.url} element={<Onboarding />} />
-            <Route path={URLData.registration.url} element={<Registration />} />
-            <Route path={URLData.signin.url} element={<Signin />} />
-            <Route
-                path={URLData.resetpassword.url}
-                element={<ResetPassword />}
-            />
-            <Route path={URLData.home.url} element={<PrivateRoute />}>
-            <Route path={URLData.home.url} element={<Home />} />
-            </Route>
-            <Route path={URLData.profile.url} element={<Profile />} />
+  return (
+    <Routes>
+      <Route path={URLData.onboarding.url} element={<Onboarding />} />
+      <Route path={URLData.profileEdit.url} element={<Onboarding />} />
+      <Route path={URLData.registration.url} element={<Registration />} />
+      <Route path={URLData.signin.url} element={<Signin />} />
+      <Route path={URLData.resetpassword.url} element={<ResetPassword />} />
+      <Route path={URLData.home.url} element={<PrivateRoute />}>
+        <Route path={URLData.home.url} element={<Home />} />
+      </Route>
+      <Route path={URLData.profile.url} element={<Profile />} />
 
-            <Route
-                path={URLData.routeRequest.url}
-                element={<RouteRequests />}
-            />
-            <Route path={URLData.chatSpace.url} element={<ChatSpace />} />
-            <Route path={URLData.emptyProfile.url} element={<EmptyProfile />} />
-        </Routes>
-    );
+      <Route path={URLData.routeRequest.url} element={<RouteRequests />} />
+      <Route path={URLData.chatSpace.url} element={<ChatSpace />} />
+      <Route path={URLData.emptyProfile.url} element={<EmptyProfile />} />
+    </Routes>
+  );
 };
 
 export default AppRouter;
