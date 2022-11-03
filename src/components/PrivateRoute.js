6cefaf30f5d0f624e52c40194cf13132bc3bf976
Anuja <83329624+anuja1694@users.noreply.github.com>
2022-11-03 14:03:44 +0000

    Router flow

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -3,14 +3,13 @@ import {Navigate, Outlet} from 'react-router-dom'
 import { useAuthStatus } from '../hooks/useAuthStatus';
 import Spinner from './Spinner/Spinner'
 
-const PrivateRoute = () => {
+const PrivateRoute = ({auth}) => {
   const { loggedIn, checkingStatus} = useAuthStatus()
   
   if(checkingStatus){
     return <Spinner />
   }
-  
-  return loggedIn ? <Outlet /> : <Navigate to = '/signin' />
+    return loggedIn ? <Outlet /> : <Navigate to = '/landing' />
 }
 
 export default PrivateRoute
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -15,25 +15,54 @@ import PrivateRoute from "../components/PrivateRoute";
 import RequestList from "../pages/requestList";
 
 const AppRouter = () => {
+ 
   return (
-    <Routes>
-      <Route path={URLData.onboarding.url} element={<Onboarding />} />
-      <Route path={URLData.profileEdit.url} element={<Onboarding />} />
-      <Route path={URLData.registration.url} element={<Registration />} />
-      <Route path={URLData.signin.url} element={<Signin />} />
-      <Route path={URLData.resetpassword.url} element={<ResetPassword />} />
-      <Route path={URLData.home.url} element={<PrivateRoute />}>
-        <Route path={URLData.home.url} element={<Home />} />
-      </Route>
-      <Route path={URLData.profile.url} element={<Profile />} />
-
-      <Route path={URLData.routeRequest.url} element={<RouteRequests />} />
-      <Route path={URLData.routeRequestList.url} element={<RequestList />} />
-      <Route path={URLData.chatSpace.url} element={<ChatSpace />} />
-      <Route path={URLData.emptyProfile.url} element={<EmptyProfile />} />
-      <Route path={URLData.landing.url} element={<Landing />} />
-    </Routes>
-  );
+        <Routes>
+            <Route path={URLData.onboarding.url} element={<PrivateRoute />}>
+                <Route path={URLData.onboarding.url} element={<Onboarding />} />
+            </Route>
+            <Route path={URLData.profileEdit.url} element={<PrivateRoute />}>
+                <Route
+                    path={URLData.profileEdit.url}
+                    element={<Onboarding />}
+                />
+            </Route>
+            <Route path={URLData.registration.url} element={<Registration />} />
+            <Route path={URLData.signin.url} element={<Signin />} />
+            <Route
+                path={URLData.resetpassword.url}
+                element={<ResetPassword />}
+            />
+            <Route path={URLData.home.url} element={<PrivateRoute />}>
+                <Route path={URLData.home.url} element={<Home />} />
+            </Route>
+            <Route path={URLData.profile.url} element={<PrivateRoute />}>
+                <Route path={URLData.profile.url} element={<Profile />} />
+            </Route>
+            <Route path={URLData.routeRequest.url} element={<PrivateRoute />}>
+                <Route
+                    path={URLData.routeRequest.url}
+                    element={<RouteRequests />}
+                />
+            </Route>
+            <Route
+                path={URLData.routeRequestList.url}
+                element={<PrivateRoute />}
+            >
+                <Route
+                    path={URLData.routeRequestList.url}
+                    element={<RequestList />}
+                />
+            </Route>
+            <Route path={URLData.chatSpace.url} element={<PrivateRoute />} >
+            <Route path={URLData.chatSpace.url} element={<ChatSpace />} />
+            </Route>
+            <Route path={URLData.emptyProfile.url} element={<PrivateRoute />} >
+            <Route path={URLData.emptyProfile.url} element={<EmptyProfile />} />
+            </Route>
+            <Route path={URLData.landing.url} element={<Landing />} />
+        </Routes>
+    );
 };
 
 export default AppRouter;
