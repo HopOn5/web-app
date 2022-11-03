8d77f0a21354f22b0ae62148fbee383feb83f7d8
Rizna PK <rizna@Riznas-MacBook-Air.local>
2022-11-03 15:23:49 +0000

    commit changes

--- src/hooks/useAuthStatus.js
+++ src/hooks/useAuthStatus.js
@@ -1,29 +1,29 @@
-import {useEffect, useState, useRef} from 'react'
-import {getAuth, onAuthStateChanged} from 'firebase/auth'
-
+import { useEffect, useState, useRef } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { setCurrentUser } from "../pages/registration/currentUserReducer";
+import { useDispatch } from "react-redux";
 
 export const useAuthStatus = () => {
-    const [loggedIn, setLoggedIn] = useState(false)
-  const [checkingStatus, setCheckingStatus] = useState(true)
-    
-  const isMounted = useRef(true)
-    
-  
-  useEffect (() => {
-    if(isMounted) {
-    const auth = getAuth()
-    onAuthStateChanged(auth, (user) => {
-        if(user) {
-            setLoggedIn(true)
+  const dispatch = useDispatch();
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [checkingStatus, setCheckingStatus] = useState(true);
+
+  const isMounted = useRef(true);
+
+  useEffect(() => {
+    if (isMounted) {
+      const auth = getAuth();
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setLoggedIn(true);
         }
-        setCheckingStatus(false)
-    })
-  }
-  return () => {
-    isMounted.current = false
-  }
-  },[isMounted])
-  
-  
-  return {loggedIn, checkingStatus}
-}
+        setCheckingStatus(false);
+      });
+    }
+    return () => {
+      isMounted.current = false;
+    };
+  }, [isMounted]);
+
+  return { loggedIn, checkingStatus };
+};
